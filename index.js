48c336733586ec61c20d58ddfe31febeaa79ebc7
kglab99 <kglab.photo@gmail.com>
2024-06-20 09:41:00 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,249 +1,229 @@
 // GLOBAL
-const tasks = document.querySelector("div#tasks");
+const tasks = document.querySelector('div#tasks')
 
 // * 1 *
 // MEMORY INITIALIZATION
-let memory = [];
-let localMemory = [];
-checkLocalStorage();
-createTasksFromMemory();
+const memory = []
+let localMemory = []
+checkLocalStorage()
+createTasksFromMemory()
 
-    // Get data from local storage if it exists
-function checkLocalStorage() {
-    
-    if (window.localStorage.length > 0) {
-        localMemory = JSON.parse(localStorage.memory);
+// Get data from local storage if it exists
+function checkLocalStorage () {
+  if (window.localStorage.length > 0) {
+    localMemory = JSON.parse(localStorage.memory)
 
-        // Runs entries through object constructor to inherit prototype
-        localMemory.forEach(item => {
-            memory.push(new ListItem(item.description, item.fav, item.done));
-        });
-
-    }
+    // Runs entries through object constructor to inherit prototype
+    localMemory.forEach(item => {
+      memory.push(new ListItem(item.description, item.fav, item.done))
+    })
+  }
 }
 
-    // Create tasks from memory 
-function createTasksFromMemory() {
-    memory.forEach(item => {
-        createTask(item.description, item.fav, item.done);
-    })
+// Create tasks from memory
+function createTasksFromMemory () {
+  memory.forEach(item => {
+    createTask(item.description, item.fav, item.done)
+  })
 }
 
 // MAIN OBJECT CONSTRUCTOR
-function ListItem(description, fav, done) {
-    this.description = description;
-    this.fav = fav;
-    this.done = done;
+function ListItem (description, fav, done) {
+  this.description = description
+  this.fav = fav
+  this.done = done
 }
 
 // DOM LISTENERS
-let addButton = document.querySelector("div#add > button").addEventListener("click", addTask);
+const addButton = document.querySelector('div#add > button').addEventListener('click', addTask)
 
 // Enter support
 document.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        addTask();
-    }
-});
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    addTask()
+  }
+})
 
 // FUNCTIONS FOR DOM LISTENERS
 
-function addTask() {
-    let description = document.querySelector("div#add > input").value;
-   
-  
-    if (description != '') {
-        if (memory.length != 0) {
-            let isValuePresent = false;
-            
-            for (const i of memory) {
-                if(i.description == description) {
-                    isValuePresent = true;
-                    document.querySelector("div#add > input").value = "";
-                    document.querySelector("div#add > input").placeholder = "Can't add duplicate task";
-                }
-            };
+function addTask () {
+  const description = document.querySelector('div#add > input').value
 
-            if (isValuePresent == false) {
-                document.querySelector("div#add > input").value = "";
-                createTask(description, false, false);
-                memory.push(new ListItem(description, false, false));
-                localStorage.memory = JSON.stringify(memory);
+  if (description != '') {
+    if (memory.length != 0) {
+      let isValuePresent = false
 
-            }
-
-
-        } else if (memory.length == 0) {
-                        document.querySelector("div#add > input").value = "";
-                        console.log(memory);
-        
-                        createTask(description, false, false);
-                        memory.push(new ListItem(description, false, false));
-                        localStorage.memory = JSON.stringify(memory);
+      for (const i of memory) {
+        if (i.description == description) {
+          isValuePresent = true
+          document.querySelector('div#add > input').value = ''
+          document.querySelector('div#add > input').placeholder = "Can't add duplicate task"
         }
+      };
 
+      if (isValuePresent == false) {
+        document.querySelector('div#add > input').value = ''
+        createTask(description, false, false)
+        memory.push(new ListItem(description, false, false))
+        localStorage.memory = JSON.stringify(memory)
+      }
+    } else if (memory.length == 0) {
+      document.querySelector('div#add > input').value = ''
+      console.log(memory)
+
+      createTask(description, false, false)
+      memory.push(new ListItem(description, false, false))
+      localStorage.memory = JSON.stringify(memory)
     }
+  }
 }
 
 // * 2 *
 // CREATING DOM TASK
 
-function createTask(taskContent, taskFav, taskDone){
+function createTask (taskContent, taskFav, taskDone) {
+  // Create main div for the task
+  const task = document.createElement('div')
 
-    //Create main div for the task
-    let task = document.createElement("div");
+  const tasksH1 = document.querySelector('h1#tasks')
+  const doneTitle = document.querySelector('h1.done')
+  const tasksTitle = document.querySelector('h1#tasks')
 
+  if (taskFav == true && taskDone == false) {
+    task.className = 'task favorite'
+    tasksH1.insertAdjacentElement('beforebegin', task)
+  } else if (taskFav == false && taskDone == false) {
+    task.className = 'task'
+    tasksH1.insertAdjacentElement('afterend', task)
+  } else if (taskFav == true && taskDone == true) {
+    task.className = 'task favorite done'
+    doneTitle.insertAdjacentElement('afterend', task)
+  } else if (taskFav == false && taskDone == true) {
+    task.className = 'task done'
+    doneTitle.insertAdjacentElement('afterend', task)
+  }
 
-    let tasksH1 = document.querySelector("h1#tasks");
-    let doneTitle = document.querySelector("h1.done");
-    let tasksTitle = document.querySelector("h1#tasks");
+  // Create right and left divs
+  const left = document.createElement('div')
+  left.className += 'left'
 
-    if(taskFav == true && taskDone == false) {
-        task.className = "task favorite";
-        tasksH1.insertAdjacentElement("beforebegin", task);
-    } else if (taskFav == false && taskDone == false) {
-        task.className = "task";
-        tasksH1.insertAdjacentElement("afterend", task);
-    } else if (taskFav == true && taskDone == true) {
-        task.className = "task favorite done";
-        doneTitle.insertAdjacentElement("afterend", task);
-    } else if (taskFav == false && taskDone == true) {
-        task.className = "task done";
-        doneTitle.insertAdjacentElement("afterend", task);
+  const right = document.createElement('div')
+  right.className += 'right'
+
+  task.append(left, right)
+
+  // Create checkbox
+  const labelCheckbox = document.createElement('label')
+  labelCheckbox.className += 'checkbox-btn'
+  left.appendChild(labelCheckbox)
+
+  const labelCheckboxInner = document.createElement('label')
+  labelCheckboxInner.setAttribute('for', 'checkbox')
+  labelCheckbox.appendChild(labelCheckboxInner)
+
+  const inputCheckbox = document.createElement('input')
+  inputCheckbox.id = 'checkbox'
+  inputCheckbox.type = 'checkbox'
+  labelCheckbox.appendChild(inputCheckbox)
+
+  const spanCheckbox = document.createElement('span')
+  spanCheckbox.className += 'checkmark'
+  labelCheckbox.appendChild(spanCheckbox)
+
+  inputCheckbox.addEventListener('click', () => {
+    for (const i in memory) {
+      if ((memory[i].description == taskContent) == true) {
+        if (memory[i].done == false) {
+          memory[i].done = true
+        } else if (memory[i].done == true) {
+          memory[i].done = false
+        }
+      }
     }
 
-    //Create right and left divs
-    let left = document.createElement("div");
-    left.className += "left";
+    localStorage.memory = JSON.stringify(memory)
 
-    let right = document.createElement("div");
-    right.className += "right";
+    if (task.className == 'task') {
+      task.className = 'task done'
+      doneTitle.insertAdjacentElement('afterend', task)
+    } else if (task.className == 'task done') {
+      task.className = 'task'
+      tasksH1.insertAdjacentElement('afterend', task)
+    } else if (task.className == 'task favorite') {
+      task.className = 'task favorite done'
+      doneTitle.insertAdjacentElement('afterend', task)
+    } else if (task.className == 'task favorite done') {
+      task.className = 'task favorite'
+      tasksH1.insertAdjacentElement('beforebegin', task)
+    }
+  })
 
-    task.append(left, right);
+  if (taskDone == true) {
+    inputCheckbox.checked = true
+  }
 
-    //Create checkbox
-    let labelCheckbox = document.createElement("label");
-    labelCheckbox.className += "checkbox-btn";
-    left.appendChild(labelCheckbox);
+  // Create task description
+  const taskText = document.createElement('p')
+  taskText.className += 'task-name'
+  taskText.textContent = taskContent
 
-    let labelCheckboxInner = document.createElement("label");
-    labelCheckboxInner.setAttribute("for", "checkbox");
-    labelCheckbox.appendChild(labelCheckboxInner);
+  left.appendChild(taskText)
 
-    let inputCheckbox = document.createElement("input");
-    inputCheckbox.id = "checkbox";
-    inputCheckbox.type = "checkbox";
-    labelCheckbox.appendChild(inputCheckbox);
+  // Add fav button
+  const favoriteBtn = document.createElement('img')
+  favoriteBtn.className += 'favorite-btn item-btn'
+  favoriteBtn.src = 'assets/favorite.svg'
 
-    let spanCheckbox = document.createElement("span");
-    spanCheckbox.className += "checkmark";
-    labelCheckbox.appendChild(spanCheckbox);
+  favoriteBtn.addEventListener('click', () => {
+    for (const i in memory) {
+      if ((memory[i].description == taskContent) == true) {
+        if (memory[i].fav == false) {
+          favoriteBtn.classList += ' checked'
 
-    inputCheckbox.addEventListener("click", () => {
-        for(const i in memory) {
-            if ((memory[i].description == taskContent) == true){
+          memory[i].fav = true
+        } else if (memory[i].fav == true) {
+          favoriteBtn.classList = 'favorite-btn item-btn'
 
-                if (memory[i].done == false) {
-                    memory[i].done = true;
-                } else if (memory[i].done == true) {
-                    memory[i].done = false;
-                }
-            }
+          memory[i].fav = false
         }
+      }
+    }
+    localStorage.memory = JSON.stringify(memory)
 
-        localStorage.memory = JSON.stringify(memory);
+    if (task.className == 'task') {
+      task.className = 'task favorite'
+      tasksH1.insertAdjacentElement('beforebegin', task)
+    } else if (task.className == 'task done') {
+      task.className = 'task favorite done'
+    } else if (task.className == 'task favorite') {
+      task.className = 'task'
+      tasksH1.insertAdjacentElement('afterend', task)
+    } else if (task.className == 'task favorite done') {
+      task.className = 'task done'
+    }
+  })
 
-        if (task.className == "task") {
-            task.className = "task done";
-            doneTitle.insertAdjacentElement("afterend", task);
-        } else if (task.className == "task done") {
-            task.className = "task";
-            tasksH1.insertAdjacentElement("afterend", task);
-        } else if (task.className == "task favorite") {
-            task.className = "task favorite done";
-            doneTitle.insertAdjacentElement("afterend", task);
-        } else if (task.className == "task favorite done") {
-            task.className = "task favorite";
-            tasksH1.insertAdjacentElement("beforebegin", task);
-        }
-    
-    })
+  if (taskFav == true) {
+    favoriteBtn.classList += ' checked'
+  }
 
-    if (taskDone == true) {
-        inputCheckbox.checked = true;
+  // Add del button
+  const deleteBtn = document.createElement('img')
+  deleteBtn.className += 'delete-btn item-btn'
+  deleteBtn.src = 'assets/trash.svg'
+
+  deleteBtn.addEventListener('click', () => {
+    for (const i in memory) {
+      if ((memory[i].description == taskContent) == true) {
+        memory.splice(i, 1)
+      }
     }
 
+    localStorage.memory = JSON.stringify(memory)
 
-    //Create task description
-    let taskText = document.createElement("p");
-    taskText.className += "task-name";
-    taskText.textContent = taskContent;
+    task.remove()
+  })
 
-    left.appendChild(taskText);
-    
-    //Add fav button
-    let favoriteBtn = document.createElement("img");
-    favoriteBtn.className += "favorite-btn item-btn";
-    favoriteBtn.src = "assets/favorite.svg";
-
-    favoriteBtn.addEventListener("click", () => {
-
-        for(const i in memory) {
-           if ((memory[i].description == taskContent) == true) {
-
-            if (memory[i].fav == false) {
-                favoriteBtn.classList += " checked";
-
-                memory[i].fav = true;
-            } else if (memory[i].fav == true) {
-                favoriteBtn.classList = "favorite-btn item-btn";
-
-                memory[i].fav = false;
-            }
-
-           }
-
-
-        }
-        localStorage.memory = JSON.stringify(memory);
-
-        if (task.className == "task") {
-            task.className = "task favorite";
-            tasksH1.insertAdjacentElement("beforebegin", task);
-        } else if (task.className == "task done") {
-            task.className = "task favorite done";
-        } else if (task.className == "task favorite") {
-            task.className = "task";
-            tasksH1.insertAdjacentElement("afterend", task);
-        } else if (task.className == "task favorite done") {
-            task.className = "task done";
-        }
-       
-    })
-
-    if (taskFav == true) {
-        favoriteBtn.classList += " checked";
-    } 
-
-    //Add del button
-    let deleteBtn = document.createElement("img");
-    deleteBtn.className += "delete-btn item-btn";
-    deleteBtn.src = "assets/trash.svg";
-
-    deleteBtn.addEventListener("click", () => {
-
-        for(const i in memory) {
-            if ((memory[i].description == taskContent) == true){
-                memory.splice(i, 1);
-
-            }
-        }
-
-        localStorage.memory = JSON.stringify(memory);
-
-        task.remove();
-    })
-
-    right.append(favoriteBtn, deleteBtn);
+  right.append(favoriteBtn, deleteBtn)
 }
